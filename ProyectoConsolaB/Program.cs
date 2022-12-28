13808bc1fbeeee1797789ea0df15b0f0322e432b
Hernando Tejada <htejadag25@gmail.com>
2022-12-28 17:29:02 -0500

    Preguntas Examen Parcial

--- ProyectoConsolaB/Program.cs
+++ ProyectoConsolaB/Program.cs
@@ -6,38 +6,101 @@ namespace ProyectoConsolaB
     class Program
     {
         static void Main(string[] args)
-        {           
+        {
             // Creación del log            
             Log.Logger = new LoggerConfiguration()
                 .WriteTo.File("log.txt")
                 .CreateLogger();
 
-            Conversion objConversion = new Conversion();
-            string valor;
+            //Conversion objConversion = new Conversion();
+            //string valor;
 
 
-            valor = null;
-            Log.Information("valor : " + valor);
-            int valorDevuelto1 = objConversion.Convertir_a_Entero(valor);
+            //valor = null;
+            //Log.Information("valor : " + valor);
+            //int valorDevuelto1 = objConversion.Convertir_a_Entero(valor);
 
-            valor = "aaa";
-            Log.Information("valor : " + valor);
-            int valorDevuelto2 = objConversion.Convertir_a_Entero(valor);
+            //valor = "aaa";
+            //Log.Information("valor : " + valor);
+            //int valorDevuelto2 = objConversion.Convertir_a_Entero(valor);
 
-            valor = "40000";
-            Log.Information("valor : " + valor);
-            int valorDevuelto3 = objConversion.Convertir_a_Entero(valor);
+            //valor = "40000";
+            //Log.Information("valor : " + valor);
+            //int valorDevuelto3 = objConversion.Convertir_a_Entero(valor);
 
             //PruebaLog objPruebaLog = new PruebaLog();
 
             //// Implementar Log del request
             //objPruebaLog.Convertir_String_Int();
             //// Implementar Log del response
-            Saludar();
+            //Saludar();
+
+            Pregunta1();
         }
 
-        public static void Saludar() {
+        public static void Saludar()
+        {
             Console.WriteLine("Hola");
+        }
+
+        public static void Pregunta1()
+        {
+            try
+            {
+                Int32 a;
+                String b = "40000";
+                a = Int32.Parse(b);
+                Console.WriteLine("El número es " + a);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static void Pregunta2()
+        {
+            try
+            {
+                int a;
+                String b = null;
+                a = int.Parse(b);
+                Console.WriteLine("El número es " + a);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+
+        public static void Pregunta3()
+        {
+            try
+            {
+                int a;
+                String b = "hola";
+                a = int.Parse(b);
+                Console.WriteLine("El número es " + a);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+
+        public static void Pregunta4()
+        {
+            int a = 5, b = 0, c;
+            c = a / b;
+            Console.WriteLine("El resultado es " + c);
+        }
+
+        public static void Pregunta5()
+        {
+            string a = "5", b = "0", c = a + b;
+            Console.WriteLine("El resultado es " + c);
         }
     }
 }
