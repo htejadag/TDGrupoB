daec87c23ea4bb658c635ada158d750ea8c661c2
Hernando Tejada <htejadag25@gmail.com>
2023-01-11 18:51:25 -0500

    Añadir data annotation a la clase Producto

--- TDB.Ms.Producto.Api/Producto.cs
+++ TDB.Ms.Producto.Api/Producto.cs
@@ -1,15 +1,16 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace TDB.Ms.Producto.Api
 {
     public class Producto
     {
         public int IdProducto { get; set; }
-
+        [Required]
         public string Nombre { get; set; }
-
+        [Required]
         public decimal Precio { get; set; }
-
+        [Required]
         public int Cantidad { get; set; }
     }
 }
