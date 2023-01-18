f305e2453c095fa2b497c4c5b5ce1d8cb92cbad1
Hernando Tejada <htejadag25@gmail.com>
2023-01-18 02:29:02 -0500

    Creación de capa Dominio

---
+++ TDB.Ms.Producto.Dominio/Entidades/Producto.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDB.Ms.Producto.Dominio.Entidades
+{
+    [BsonIgnoreExtraElements]
+    public class Producto
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string _id { get; set; }
+        public int IdProducto { get; set; }
+
+        public string Nombre { get; set; }
+
+        public decimal Precio { get; set; }
+
+        public int Cantidad { get; set; }
+    }
+}
