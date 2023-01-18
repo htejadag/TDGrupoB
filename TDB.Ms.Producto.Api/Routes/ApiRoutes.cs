4f24f9f403d797459531df67ad5344b34eef5c9a
Hernando Tejada <htejadag25@gmail.com>
2023-01-17 21:34:25 -0500

    Creación de ApiRoutes

---
+++ TDB.Ms.Producto.Api/Routes/ApiRoutes.cs
@@ -0,0 +1,21 @@
+﻿namespace TDB.Ms.Producto.Api.Routes
+{
+    public static class ApiRoutes
+    {
+        public const string Root = "api";
+        public const string Version = "v1";
+        public const string Base = Root + "/" + Version;
+
+        public static class Producto
+        {
+            // Read
+            public const string GetAll = Base + "/producto/all";
+            public const string GetById = Base + "/producto/{id}";
+
+            // Write
+            public const string Create = Base + "/producto/create";
+            public const string Update = Base + "/producto/update";
+            public const string Delete = Base + "/producto/delete";
+        }
+    }
+}
