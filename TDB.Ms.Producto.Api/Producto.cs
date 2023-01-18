ce9a0d7a9afb31ade1a146117d8bb6598ad0e88c
Hernando Tejada <htejadag25@gmail.com>
2023-01-17 23:09:33 -0500

    Agregar tipo de dato Bson a clase producto

--- TDB.Ms.Producto.Api/Producto.cs
+++ TDB.Ms.Producto.Api/Producto.cs
@@ -1,3 +1,5 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 using System;
 using System.ComponentModel.DataAnnotations;
 
@@ -5,6 +7,9 @@ namespace TDB.Ms.Producto.Api
 {
     public class Producto
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string _id { get; set; }
         public int IdProducto { get; set; }
         [Required]
         public string Nombre { get; set; }
