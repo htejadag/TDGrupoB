eff33e225731bafdc0cc43240c663799858e375a
Hernando Tejada <htejadag25@gmail.com>
2023-01-18 18:21:05 -0500

    Actualizar capa dominio

---
+++ TDB.Ms.Producto.Dominio/Entidades/Categorias.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDB.Ms.Producto.Dominio.Entidades
+{
+    public class Categorias
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string _id { get; set; } 
+    }
+}
---
+++ TDB.Ms.Producto.Dominio/Servicios/ProductoService.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TDB.Ms.Producto.Infraestructura.DBSettings;
+using dominio = TDB.Ms.Producto.Dominio.Entidades;
+
+namespace TDB.Ms.Producto.Dominio.Servicios
+{
+    public class ProductoService
+    {
+        private IMongoCollection<dominio.Producto> _producto;
+
+        public ProductoService(IDBSettings dBSettings)
+        {
+            var cliente = new MongoClient(dBSettings.Server);
+            var database = cliente.GetDatabase(dBSettings.Database);
+            _producto = database.GetCollection<dominio.Producto>(dBSettings.Collection);
+        }
+
+        public List<dominio.Producto> ListarProductos()
+        {
+            return _producto.Find(x => true).ToList();
+        }
+    }
+}
