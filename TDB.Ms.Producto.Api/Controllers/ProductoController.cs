25df2e2a8de29647ac25f1166521ff863e4fc069
Hernando Tejada <htejadag25@gmail.com>
2023-01-17 23:09:20 -0500

    Configuración de rutas de Producto

--- TDB.Ms.Producto.Api/Controllers/ProductoController.cs
+++ TDB.Ms.Producto.Api/Controllers/ProductoController.cs
@@ -3,46 +3,58 @@ using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using TDB.Ms.Producto.Api.Routes;
 
 namespace TDB.Ms.Producto.Api.Controllers
 {
     [ApiController]
-    [Route("productos")]
     public class ProductoController : ControllerBase
     {
-        private static List<Producto> listaProducto = new List<Producto>
+        //private static List<Producto> listaProducto = new List<Producto>
+        //{
+        //    new Producto
+        //    {
+        //        IdProducto = 1,
+        //        Nombre = "Arroz",
+        //        Cantidad = 100,
+        //        Precio = 5.5M
+        //    },
+
+        //    new Producto
+        //    {
+        //        IdProducto = 2,
+        //        Nombre = "Leche",
+        //        Cantidad = 80,
+        //        Precio = 4.4M
+        //    }
+        //};
+
+
+        [HttpGet(ApiRoutes.Producto.GetAll)]
+        public IEnumerable<Producto> ListarProductos()
         {
-            new Producto
-            {
-                IdProducto = 1,
-                Nombre = "Arroz",
-                Cantidad = 100,
-                Precio = 5.5M
-            },
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("TDC_productos");
+            var dbProducto = database.GetCollection<Producto>("producto");
 
-            new Producto
-            {
-                IdProducto = 2,
-                Nombre = "Leche",
-                Cantidad = 80,
-                Precio = 4.4M
-            }
-        };
+            var listaProducto = dbProducto.Find(x=>true).ToList();
 
-
-        //[HttpGet(Name = "ListarProductos")]
-        //public IEnumerable<Producto> ListarProductos()
-        //{           
-        //    return listaProducto;
-        //}
-
-        [HttpGet(Name = "BuscarProducto")]
-        public Producto BuscarProducto(int id)
-        {
-            return listaProducto.Single(x => x.IdProducto == id);
+            return listaProducto;
         }
 
-        [HttpPost(Name = "CrearProducto")]
+        [HttpGet(ApiRoutes.Producto.GetById)]
+        public Producto BuscarProducto(int id)
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("TDC_productos");
+            var dbProducto = database.GetCollection<Producto>("producto");
+
+            var objProducto = dbProducto.Find(x => x.IdProducto == id).FirstOrDefault();
+
+            return objProducto;
+        }
+
+        [HttpPost(ApiRoutes.Producto.Create)]
         public ActionResult<Producto> CrearProducto(Producto producto)
         {
             var client = new MongoClient("mongodb://localhost:27017");
@@ -54,20 +66,21 @@ namespace TDB.Ms.Producto.Api.Controllers
             return CreatedAtAction("CrearProducto", producto);
         }
 
-        [HttpPut(Name = "ModificarProducto")]
+        [HttpPut(ApiRoutes.Producto.Update)]
         public ActionResult<Producto> ModificarProducto(Producto producto)
         {
-            Producto productoModificado = listaProducto.Single(x => x.IdProducto == producto.IdProducto);
-            productoModificado.Nombre = producto.Nombre;
-            productoModificado.Cantidad = producto.Cantidad;
-            productoModificado.Precio= producto.Precio;
-            return CreatedAtAction("ModificarProducto", productoModificado);
+            //Producto productoModificado = listaProducto.Single(x => x.IdProducto == producto.IdProducto);
+            //productoModificado.Nombre = producto.Nombre;
+            //productoModificado.Cantidad = producto.Cantidad;
+            //productoModificado.Precio= producto.Precio;
+            //return CreatedAtAction("ModificarProducto", productoModificado);
+            return Ok();
         }
 
-        [HttpDelete(Name = "EliminarProducto")]
+        [HttpDelete(ApiRoutes.Producto.Delete)]
         public ActionResult<Producto> EliminarProducto(int id)
         {
-            listaProducto.RemoveAt(id);
+            //listaProducto.RemoveAt(id);
             return Ok(id);
         }
     }
